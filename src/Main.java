d0640e3005c87a5eac4bef8d5a2e083a158786ec
ASUS <successolga118@gmail.com>
2025-05-24 18:06:55 +0300

    2 task

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,30 @@
 // Jeff,jeff@mail.com,23;Jack,jack@mail.com,45;Nick,
 // nick@mail.com,24;Oleg,oleg@mail.com,22
 
-
+// https://github.com/Algushka/FileOutputStream.git
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-      String str = "Jeff,jeff@mail.com,23; Jack, jack@mail.com, 45; Nick, nick@mail.com, 24; Oleg, oleg@mail.com,22";
-      try{
-        FileOutputStream fileOutputStream = new FileOutputStream("example.txt");
-      for (var b:str.getBytes()) {
-          fileOutputStream.write(b);
-      }}
-      catch (IOException e) {
-          System.out.println("File not found");
+        String str = "Jeff,jeff@mail.com,23; Jack, jack@mail.com, 45; Nick, nick@mail.com, 24; Oleg, oleg@mail.com,22";
+        FileOutputStream fileOutputStream=null;
+        try {
+            fileOutputStream = new FileOutputStream("example.txt");
+            for (var b : str.getBytes()) {
+                fileOutputStream.write(b);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found");
 
-      }
+        } finally {
+            try {
+                fileOutputStream.close();
+            } catch (Exception e) {
+                System.out.println("не получилось закрыть поток");
+            }
+
+        }
     }
-    }
+}
---
+++ src/Main2.java
@@ -0,0 +1,47 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Main2 {
+    public static void main(String[] args) {
+        String str=ReadFromFile("example.txt");
+        WriteToFile(str);
+
+    }
+    public  static String ReadFromFile(String nameOfFile)
+    { StringBuilder sb=null;
+        try ( FileInputStream fileInputStream = new FileInputStream("example.txt");){
+        sb = new StringBuilder();
+        int data;
+        while ((data = fileInputStream.read())!=-1) {
+            sb.append ((char)data);
+        }
+        System.out.println(sb.toString());
+    } catch (IOException e) {
+        System.out.println("Mistakes!!!!");
+    }
+    return  sb.toString();
+    }
+    public static void WriteToFile(String str){
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream("example2.txt")) {
+            for (var b: str.getBytes()){
+                fileOutputStream.write(b);
+            }
+
+        }
+        catch (IOException e) {
+            System.out.println("error");
+        }
+        //finally{
+        //    try {
+        //        fileOutputStream.close();
+        //    } catch (Exception e) {
+        //        System.out.println("не получилось закрыть поток");
+           // }
+        }
+
+
+    }
+
