ea47720a9ef2cc54b0c53a28afc3f50ac2d5ed44
ASUS <successolga118@gmail.com>
2025-05-25 18:51:30 +0300

    3 task read the line from the file

---
+++ src/Main3.java
@@ -0,0 +1,52 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main3 {
+    public static void main(String[] args) {
+ReadFromFile("example.txt");
+    }
+    public static String ReadFromFile(String nameOfFile) {
+StringBuilder sb = null;
+        try ( FileInputStream fileInputStream = new FileInputStream(nameOfFile);){
+        sb= new StringBuilder();
+        int data;
+        int counterOfCommas=0;
+        int counterOfPersons=0;
+      boolean firstElement = true;
+        while ((data = fileInputStream.read())!=-1) {
+            if (!firstElement) sb.append(';');
+            counterOfPersons++;
+            //System.out.println("Количество человек" + counterOfPersons);
+            if ((firstElement)||(counterOfCommas==2)){
+            counterOfCommas=0;
+            firstElement=false;}
+            else {
+                System.out.println("Not enough fields, add fields to: " +counterOfPersons+ "   Person");
+                throw new RuntimeException();
+
+            }
+            while ((((char)data)!=';')) // && (data = fileInputStream.read())!=-1){
+            {
+                //System.out.println(counterOfCommas);
+                if (((char)data) ==',')  {counterOfCommas++;}
+                System.out.println((char)data);
+                sb.append((char) data);
+            data = fileInputStream.read();
+
+            }
+            System.out.println(sb.toString());
+        }
+
+    }
+        catch (IOException e ){
+            System.out.println("Error");
+        }
+        finally {
+            System.out.println("Всем привет !");
+        }
+        return sb.toString();
+    }
+
+}
+
---
+++ src/Person.java
@@ -0,0 +1,45 @@
+public class Person {
+    private String name;
+    private String email;
+    private int age;
+
+    public Person(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Person{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", age=").append(age);
+        sb.append('}');
+        return sb.toString();
+    }
+}
