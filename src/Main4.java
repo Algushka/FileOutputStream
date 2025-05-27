565f9f6d966127660984aa97b77a2056b95febc3
ASUS <successolga118@gmail.com>
2025-05-27 14:05:46 +0300

    4 task

---
+++ src/Main4.java
@@ -0,0 +1,112 @@
+//Доработайте последнюю задачу таким образом,
+// что бы система пропускала 'некорректных' персонов:
+//
+//не все поля заданны
+//не корректно задан возраст
+//еmail не содержит @
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main4 {
+    public static void main(String[] args) throws MyException{
+
+       // System.out.println(checkAddInEmail("fdhsjfk"));
+        String str=ReadFromFile("example.txt");
+        //addToPerson(str);
+        List<Person> list = formPersonListFromString(str);
+        printPersonList(list);
+    }
+
+    public static String ReadFromFile(String nameOfFile) {
+StringBuilder sb = null;
+        try ( FileInputStream fileInputStream = new FileInputStream(nameOfFile);){
+        sb= new StringBuilder();
+        int data;
+        int counterOfCommas=0;
+        int counterOfPersons=0;
+      boolean firstElement = true;
+        while ((data = fileInputStream.read())!=-1) {
+            if (!firstElement) sb.append(',');
+            counterOfPersons++;
+            //System.out.println("Количество человек" + counterOfPersons);
+            if ((firstElement)||(counterOfCommas==2)){
+            counterOfCommas=0;
+            firstElement=false;}
+            else {
+                System.out.println("Not enough fields, add fields to: " +counterOfPersons+ "   Person");
+                continue;
+               // throw new RuntimeException(); continue;
+
+            }
+            while ((((char)data)!=';')) // && (data = fileInputStream.read())!=-1){
+            {
+                //System.out.println(counterOfCommas);
+                if (((char)data) ==',')  {counterOfCommas++;}
+                System.out.println((char)data);
+                sb.append((char) data);
+            data = fileInputStream.read();
+
+            }
+            System.out.println(sb.toString());
+        }
+
+    }
+        catch (IOException e ){
+            System.out.println("Error");
+        }
+        finally {
+            System.out.println("Всем привет !");
+        }
+        return sb.toString();
+    }
+    public static boolean checkAddInEmail (String email) {
+        return email.contains("@");
+    }
+    public static void addToPerson(String str) {
+        String[]strings=str.split(",");
+        for (String element:strings) {
+            System.out.println(element);
+        }
+    }
+    public static List<Person> formPersonListFromString(String str) throws MyException {
+        List <Person> list =new ArrayList<>() ;
+        String[]strings=str.split(",");
+        System.out.println("длина массива строк"+ strings.length);
+        for (int i=0; i< strings.length-2; i=i+3){
+              //  int index=0;
+                if (checkAddInEmail (strings[i+1])!=true)
+                {
+                    //System.out.println("ээээээ"+strings[i+1]);
+                    strings[i+1]="@";
+                }
+
+
+               try {int age=Integer.parseInt(strings[i+2]);
+                   Person person = new Person(strings[i], strings[i + 1], age);
+                  // System.out.println(person);
+                   list.add(person);
+                }
+               catch (Exception e){
+                   System.out.println("Not proper format of age");
+                   int age =0;
+                   Person person = new Person(strings[i], strings[i + 1], age);
+                  // System.out.println(person);
+                   list.add(person);
+                   continue;
+           }
+         //   Person person = new Person(strings[i], strings[i + 1], Integer.parseInt(strings[i + 2]));
+        //    System.out.println(person);
+        //    list.add(person);
+        }
+        return list;
+    }
+
+    public static void printPersonList(List<Person> list) {
+        for (Person element: list ) {
+            System.out.println(element);
+        }
+    }
+}
+
